21d4ec801dfecb18b1df0ef4020ba7835fc6fefc
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-09 00:36:39 +0100

    Tomtom map initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+
+  useEffect( ()=> {
+
+    let map = tt.map({
+      key: import.meta.env.VITE_REACT_TOMTOM_API_KEY,
+      container: "map",
+    })
+
+
+  }, []);
+
 
   return (
 
