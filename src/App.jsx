a7f7927bf166d7fd7d6dc1390b598d5078e4b595
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-18 13:38:11 +0400

    Fetch API with proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
 import * as tt from '@tomtom-international/web-sdk-maps';
 
 function App() {
 
+  const mapElement = useRef();
   const [map, setMap] = useState({});
 
-
-  useEffect( ()=> {
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_REACT_TOMTOM_API_KEY;
+    const url = `https://api.tomtom.com/map/2/copyrights/caption.json?key=${apiKey}`;
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
     let map = tt.map({
-      key: import.meta.env.VITE_REACT_TOMTOM_API_KEY,
-      container: "map",
-    })
+      key: apiKey,
+      container: mapElement.current,
+    });
 
     setMap(map);
-
-
   }, []);
 
-
   return (
-
-    <div></div>
-    
-  )
+    <div>
+      <div ref={mapElement}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
